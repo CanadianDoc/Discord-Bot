38e661590eb595ac23c2a7830d5a26fa74380bf6
CanadianDoc <78990604+CanadianDoc@users.noreply.github.com>
2023-08-16 22:40:03 -0600

    attendance + poll

--- src/events/bot/interactionCreate.js
+++ src/events/bot/interactionCreate.js
@@ -1,37 +1,61 @@
 const { GatewayIntentBits } = require("discord.js");
 
 module.exports = {
-	name: "interactionCreate",
-	async execute(interaction, bot) {
-		if (interaction.isChatInputCommand()) {
-			const { commands } = bot;
-			const { commandName } = interaction;
-			const command = commands.get(commandName);
-			if (!command) return;
+  name: "interactionCreate",
+  async execute(interaction, bot) {
+    if (interaction.isChatInputCommand()) {
+      const { commands } = bot;
+      const { commandName } = interaction;
+      const command = commands.get(commandName);
+      if (!command) return;
 
-			try {
-				await command.execute(interaction, bot);
-			} catch (err) {
-				console.error(err);
-				await interaction.reply({
-					content: `Something went wrong while executing this command, please inform Doc about it thank you!`,
-					ephemeral: true,
-				});
-			}
-		} else if (interaction.isButton()) {
-			const { buttons } = bot;
-			const { customId } = interaction;
-			const button = buttons.get(customId);
-			if (!button) {
-				console.log("No customId associated with this button");
-				return new Error("No customId associated with this button");
-			}
+      try {
+        await command.execute(interaction, bot);
+      } catch (err) {
+        console.error(err);
+        await interaction.reply({
+          content: `Something went wrong while executing this command, please inform Doc about it thank you!`,
+          ephemeral: true,
+        });
+      }
+    } else if (interaction.isButton()) {
+      const { customId } = interaction;
+      const [type, action, id] = customId.split("-");
 
-			try {
-				await button.execute(interaction, bot);
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	},
+      let buttonHandler;
+
+      switch (type) {
+        case "poll":
+          {
+            buttonHandler = require("../buttons/pollButton.js");
+          }
+          break;
+
+        case "attendance":
+          {
+            buttonHandler = require("../buttons/attendanceButton.js");
+          }
+          break;
+
+        case "role":
+          {
+            buttonHandler = require("../buttons/roleButton.js");
+          }
+          break;
+
+        default:
+          {
+            console.log("No customId associated with this button");
+            return new Error("No customId associated with this button");
+          }
+          break;
+      }
+
+      try {
+        await buttonHandler.execute(interaction, bot);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
 };
